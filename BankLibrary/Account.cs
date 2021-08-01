ac8a1361455db56f8d24e8d2f2872acfbda57373
Pavlov Vladimir <bob.pavlov.a7@gmail.com>
2021-08-01 20:08:51 +0300

    Added classes: abstract Account, DemendAccount and DepositAccount. Added AccountStateHandler.

---
+++ BankLibrary/Account.cs
@@ -0,0 +1,95 @@
+﻿namespace BankLibrary
+{
+    public abstract class Account : IAccount
+    {
+        protected internal event AccountStateHandler Withdrawed;
+        protected internal event AccountStateHandler Added;
+        protected internal event AccountStateHandler Opened;
+        protected internal event AccountStateHandler Closed;
+        protected internal event AccountStateHandler Calculated;
+
+        static int _counter = 0;
+        protected int _days = 0;
+        public decimal Sum { get; private set; }
+        public int Percentage { get; private set; }
+        public int Id { get; private set; }
+
+        public Account(decimal sum, int percentage)
+        {
+            Sum = sum;
+            Percentage = percentage;
+            Id = ++_counter;
+        }
+
+        private void CallEvent(AccountEventArgs e, AccountStateHandler handler)
+        {
+            if (e != null)
+                handler?.Invoke(this, e);
+        }
+
+        protected virtual void OnOpened(AccountEventArgs e)
+        {
+            CallEvent(e, Opened);
+        }
+        protected virtual void OnWithdrawed(AccountEventArgs e)
+        {
+            CallEvent(e, Withdrawed);
+        }
+        protected virtual void OnAdded(AccountEventArgs e)
+        {
+            CallEvent(e, Added);
+        }
+        protected virtual void OnClosed(AccountEventArgs e)
+        {
+            CallEvent(e, Closed);
+        }
+        protected virtual void OnCalculated(AccountEventArgs e)
+        {
+            CallEvent(e, Calculated);
+        }
+
+        public virtual void Put(decimal sum)
+        {
+            Sum += sum;
+            OnAdded(new AccountEventArgs($"The amount {sum} has been credited to the account", sum));
+        }
+
+        public virtual decimal Withdraw(decimal sum)
+        {
+            decimal result = 0;
+            if (Sum >= sum)
+            {
+                Sum -= sum;
+                result = sum;
+                OnWithdrawed(new AccountEventArgs($"The amount {sum} has been withdrawn from the account {Id}", sum));
+            }
+            else
+            {
+                OnWithdrawed(new AccountEventArgs($"Not enough money in the account {Id}", 0));
+            }
+            return result;
+        }
+
+        protected internal virtual void Open()
+        {
+            OnOpened(new AccountEventArgs($"New account opened. Account ID: {Id}", Sum));
+        }
+
+        protected internal virtual void Close()
+        {
+            OnClosed(new AccountEventArgs($"The account {Id} is closed. Total amount: {Sum}", Sum));
+        }
+
+        protected internal void IncrementDays()
+        {
+            _days++;
+        }
+
+        protected internal virtual void Calculate()
+        {
+            decimal increment = Sum * Percentage / 100;
+            Sum += increment;
+            OnCalculated(new AccountEventArgs($"Interest accrued: {increment}", increment));
+        }
+    }
+}
---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace BankLibrary
+{
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    public class AccountEventArgs
+    {
+        public string Message { get; private set; }
+        public decimal Sum { get; private set; }
+
+        public AccountEventArgs (string message, decimal sum)
+        {
+            Message = message;
+            Sum = sum;
+        }
+    }
+}
